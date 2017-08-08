22539ff02baa69309180781c332cbe4e1907d7c6
Tleu13 <tleuzhan13@gmail.com>
2017-08-08 09:24:05 +0600

    Update database.sql

--- src/main/resources/database/database.sql
+++ src/main/resources/database/database.sql
@@ -8,8 +8,6 @@ CREATE DATABASE "epamADSM"
   WITH OWNER = "epamAdmin"
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
 
 
